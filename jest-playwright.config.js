592f74aaf176e118d74b1c7683817e66e3db74b8
KATT <alexander@n1s.se>
2021-08-13 12:43:40 +0200

    assume fine if works in chromium

--- jest-playwright.config.js
+++ jest-playwright.config.js
@@ -1,6 +1,6 @@
 // https://github.com/playwright-community/jest-playwright/#configuration
 module.exports = {
-  browsers: ['chromium', 'firefox', 'webkit'],
+  browsers: ['chromium'],
   exitOnPageError: false, // GitHub currently throws errors
   launchOptions: {
     headless: true,
